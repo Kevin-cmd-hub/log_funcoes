62be5c93a60f592e38c398f1474bacbd8fda6426
Kevin-cmd-hub <kevinhenryky10@gmail.com>
2026-04-23 14:41:08 -0300

    v.0.1 - Acrescentado funções da biblioteca.

--- main.php
+++ main.php
@@ -1,29 +1,29 @@
 <?php
 
 //funçoes em PHP
-echo "Na aula do dia 23/04/2026 eu aprendi a mexer no GitHub/Git e desenvolver funçoes no PHP! =D";
+echo "Na aula do dia 23/04/2026 eu aprendi a mexer no GitHub/Git e desenvolver funçoes no PHP! =D \n";
 
 function Somar($a, $b, $c){
     return $a + $b + $c;
 }
 
-echo "Resultado: ", Somar(10,5,3); 
+echo "Resultado: ", Somar(10,5,3), "\n"; 
 
 
 function Subtrair($a, $b){
     return $a - $b;
 }
 
-echo "Resultado: ", Subtrair(10,5);
+echo "Resultado: ", Subtrair(10,5), "\n";
 
 function Dividir($a, $b){
     return $a / $b;
 }
 
-echo "Resultado: ", Dividir(10,5);
+echo "Resultado: ", Dividir(10,5), "\n";
 
 function Multiplicar($a, $b){
     return $a * $b;
 }
 
-echo "Resultado: ", Multiplicar(10,5);
+echo "Resultado: ", Multiplicar(10,5),"\n";
