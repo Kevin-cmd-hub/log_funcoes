c69dd2f4e049343c9e3a16a6422c37140015a8fb
Kevin-cmd-hub <kevinhenryky10@gmail.com>
2026-04-23 11:43:45 -0300

    v.0.2 - Acrescentado função de somar.

--- main.php
+++ main.php
@@ -2,3 +2,10 @@
 
 //funçoes em PHP
 echo "Na aula do dia 23/04/2026 eu aprendi a mexer no GitHub/Git e desenvolver funçoes no PHP! =D";
+
+function Somar($a, $b, $c){
+    return $a + $b + $c;
+}
+
+echo "Resultado: ", Somar(10,5,3); 
+
