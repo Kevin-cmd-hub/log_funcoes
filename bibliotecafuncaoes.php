307f5d31c0825497ff11823b1b47400e7afc745b
Kevin-cmd-hub <kevin.h.costa@edu.senai.br>
2026-04-28 09:54:15 -0300

    v.0.2 - Adicionado a função de conversão para Real

---
+++ bibliotecafuncaoes.php
@@ -0,0 +1,30 @@
+<?php
+
+//Namespace conversao:
+// Função dolarParaReal($valor, $cotacao)
+// Função euroParaReal($valor, $cotacao)
+// Função pesoParaReal($valor, $cotacao)
+// Função libraParaReal($valor, $cotacao)
+// Função ieneParaReal($valor, $cotacao)
+
+namespace conversao{
+    function dolarParaReal($valor, $cotacao){
+        return $valor * $cotacao;
+    }
+
+    function euroParaReal($valor, $cotacao){
+        return $valor * $cotacao;
+    }
+
+    function pesoParaReal($valor, $cotacao){
+        return $valor * $cotacao;
+    }
+
+    function libraParaReal($valor, $cotacao){
+        return $valor * $cotacao;
+    }
+
+    function ieneParaReal($valor, $cotacao){
+        return $valor * $cotacao;
+    }
+}
