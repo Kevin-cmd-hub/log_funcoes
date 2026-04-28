a674f8e5e05d0c5af51ff2ecf644bf76bca7990d
Kevin-cmd-hub <kevin.h.costa@edu.senai.br>
2026-04-28 12:11:32 -0300

    v.0.1 - Adicionado a função de conversão e calcular

--- entradaDadosGeometria.php
+++ entradaDadosGeometria.php
@@ -8,8 +8,8 @@ use function conversao\frequenciaCardiacaMaxima;
 use function conversao\converterLibrasParaQuilo;
 use function conversao\calcularCaloriasBasais;
 
-echo "O lmc deu: ", calcularImc (100, 5);
-echo "\nO valor ideal de água é: ", valorIdealAgua (100, 5);
-echo "\nA frequencia cardiaca máxima é: ", frequenciaCardiacaMaxima (100, 5);
-echo "\nA conversão deu: ", converterLibrasParaQuilo (100, 5);
+echo "O lmc deu: ", calcularImc (50, 4);
+echo "\nO valor ideal de água é: ", valorIdealAgua (65);
+echo "\nA frequencia cardiaca máxima é: ", frequenciaCardiacaMaxima (10, 15);
+echo "\nA conversão deu: ", converterLibrasParaQuilo (58);
 echo "\nO tanto de calorias basais é: ", calcularCaloriasBasais (50, 1.75, 15, "Masculino");
